3356c905577861a159c22bd3996e85cab9d73d11
Vui Nguyen <vui_engineer@yahoo.com>
2018-12-19 16:21:07 -0700

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Vui Nguyen on 12/19/18.
+//  Copyright © 2018 Sunfish Empire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
